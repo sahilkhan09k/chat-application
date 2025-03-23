cbb9a5044c6fd787d2310110b00f68a1b4cd561e
sahilkhan09k <sahilkhan392005@gmail.com>
2025-03-23 22:33:58 +0530

    refactor: simplify App component structure and improve readability

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,35 +7,33 @@ import LoginPage from './pages/LoginPage.jsx'
 import SettingPage from './pages/SettingPage.jsx'
 import ProfilePage from './pages/ProfilePage.jsx'
 import { useAuthStore } from './store/useAuthStore.js'
-import { Loader } from 'lucide-react'
-import { Toaster } from 'react-hot-toast'
 import { useThemeStore } from './store/useThemeStore.js'
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
-function App() {
-  const {authUser, checkAuth, isCheckingAuth, onlineUsers} = useAuthStore();
-  console.log("OnlineUsers", onlineUsers)
-  const {theme, setTheme} = useThemeStore();
+const App = () => {
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+  const { theme } = useThemeStore();
+
+  console.log({ onlineUsers });
 
   useEffect(() => {
-    document.documentElement.setAttribute("data-theme", theme);
-  }, [theme]);
+    checkAuth();
+  }, [checkAuth]);
 
-  useEffect(() => {
-      checkAuth();
-  }, [checkAuth])
+  console.log({ authUser });
 
-  console.log({authUser})
-
-  if(isCheckingAuth && !authUser) {
-    return(
-      <div className='flex items-center justify-center h-screen'>
-        <Loader className='size-10  animate-spin' />
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
       </div>
-    )
-  }
+    );
+
   return (
-    <div data-theme = {theme}>
+    <div data-theme={theme}>
       <Navbar />
+
       <Routes>
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
         <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
@@ -43,9 +41,9 @@ function App() {
         <Route path="/settings" element={<SettingPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
+
       <Toaster />
     </div>
-  )
-}
-
-export default App
+  );
+};
+export default App;
